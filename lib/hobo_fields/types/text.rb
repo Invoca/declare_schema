a1f7e9f07ae48b437cfeb330653aaf040aa18a25
Colin Kelley <colindkelley@gmail.com>
2020-06-29 09:41:45 -0700

    TECH-4526: add .html_safe since gsub strips that away

--- lib/hobo_fields/types/text.rb
+++ lib/hobo_fields/types/text.rb
@@ -8,7 +8,7 @@ module HoboFields
       COLUMN_TYPE = :text unless defined? COLUMN_TYPE
 
       def to_html(xmldoctype = true)
-        ERB::Util.html_escape(self).gsub("\n", "<br#{xmldoctype ? ' /' : ''}>\n")
+        ERB::Util.html_escape(self).gsub("\n", "<br#{xmldoctype ? ' /' : ''}>\n").html_safe
       end
 
       HoboFields.register_type(:text, self)
