9db166e3ed7b801f1e2b15221910df7c1ee43536
Colin Kelley <colindkelley@gmail.com>
2020-10-11 07:46:30 -0700

    TECH-4872: remove ERB since it fails on travis sometimes with TypeError: no implicit conversion of Hash into Integer

--- lib/generators/declare_schema/support/model.rb
+++ lib/generators/declare_schema/support/model.rb
@@ -4,6 +4,39 @@ require_relative './eval_template'
 
 module DeclareSchema
   module Support
+    class IndentedBuffer
+      def initialize(indent: 0)
+        @string = +""
+        @indent = indent
+        @column = 0
+        @indent_amount = 2
+      end
+
+      def to_string
+        @string
+      end
+
+      def indent!
+        @indent += @indent_amount
+        yield
+        @indent -= @indent_amount
+      end
+
+      def newline!
+        @column = 0
+        @string << "\n"
+      end
+
+      def <<(str)
+        if (difference = @indent - @column) > 0
+          @string << ' ' * difference
+        end
+        @column += difference
+        @string << str
+        newline!
+      end
+    end
+
     module Model
       class << self
         def included(base)
@@ -27,8 +60,50 @@ module DeclareSchema
             def inject_declare_schema_code_into_model_file
               gsub_file(model_path, /  # attr_accessible :title, :body\n/m, "")
               inject_into_class(model_path, class_name) do
-                eval_template('model_injection.rb.erb')
+                declare_model_fields_and_associations
               end
+            end
+
+            private
+
+            def declare_model_fields_and_associations
+              buffer = ::DeclareSchema::Support::IndentedBuffer.new(indent: 2)
+              buffer.newline!
+              buffer << 'fields do'
+              buffer.indent! do
+                field_attributes.each do |attribute|
+                  decl = "%-#{max_attribute_length}s" % attribute.name + ' ' +
+                    attribute.type.to_sym.inspect +
+                    case attribute.type.to_s
+                    when 'string'
+                      ', limit: 255'
+                    else
+                      ''
+                    end
+                  buffer << decl
+                end
+                if options[:timestamps]
+                  buffer.newline!
+                  buffer << 'timestamps'
+                end
+              end
+              buffer << 'end'
+
+              if bts.any?
+                buffer.newline!
+                bts.each do |bt|
+                  buffer << "belongs_to #{bt.to_sym.inspect}"
+                end
+              end
+              if hms.any?
+                buffer.newline
+                hms.each do |hm|
+                  buffer << "has_many #{hm.to_sym.inspect}, dependent: :destroy"
+                end
+              end
+              buffer.newline!
+
+              buffer.to_string
             end
 
             protected
