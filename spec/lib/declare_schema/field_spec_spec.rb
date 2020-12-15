c71d76052edfcfb593817e0b87ba0d7191f36fe6
Yinon Rousso <yrousso@invoca.com>
2020-12-15 10:53:05 -0800

    TECH-4971 add all 4 test permutations for bigint and int8 column changes

--- spec/lib/declare_schema/field_spec_spec.rb
+++ spec/lib/declare_schema/field_spec_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'DeclareSchema Model FieldSpec' do
     load File.expand_path('prepare_testapp.rb', __dir__)
   end
   context 'There are no model columns to change' do
-    it '#different_to should return false for integer field' do
+    it '#different_to should return false for int8 == int8' do
       class Advert < ActiveRecord::Base
         fields do
           price :integer, limit: 8
@@ -20,7 +20,7 @@ RSpec.describe 'DeclareSchema Model FieldSpec' do
       expect(subject.different_to?(col)).to eq(false)
     end
 
-    it '#different_to should return false for bigint field' do
+    it '#different_to should return false for bigint == bigint' do
       class Advert < ActiveRecord::Base
         fields do
           price :bigint
@@ -29,7 +29,36 @@ RSpec.describe 'DeclareSchema Model FieldSpec' do
 
       subject = DeclareSchema::Model::FieldSpec.new(Advert, :price, :bigint, { null: false })
 
-      sql_type_metadata = ActiveRecord::ConnectionAdapters::SqlTypeMetadata.new(sql_type: "bigint", type: :bigint)
+      sql_type_metadata = ActiveRecord::ConnectionAdapters::SqlTypeMetadata.new(sql_type: "bigint(20)", type: :bigint)
+      col = ActiveRecord::ConnectionAdapters::Column.new("price", nil, sql_type_metadata, false, "adverts")
+
+      expect(subject.different_to?(col)).to eq(false)
+    end
+
+    it '#different_to should return false for int8 == bigint' do
+      class Advert < ActiveRecord::Base
+        fields do
+          price :integer, limit: 8
+        end
+      end
+
+      subject = DeclareSchema::Model::FieldSpec.new(Advert, :price, :integer, { limit: 8, null: false })
+
+      sql_type_metadata = ActiveRecord::ConnectionAdapters::SqlTypeMetadata.new(sql_type: "bigint(20)", type: :bigint)
+      col = ActiveRecord::ConnectionAdapters::Column.new("price", nil, sql_type_metadata, false, "adverts")
+      expect(subject.different_to?(col)).to eq(false)
+    end
+
+    it '#different_to should return false for bigint == int8' do
+      class Advert < ActiveRecord::Base
+        fields do
+          price :bigint
+        end
+      end
+
+      subject = DeclareSchema::Model::FieldSpec.new(Advert, :price, :bigint, { null: false })
+
+      sql_type_metadata = ActiveRecord::ConnectionAdapters::SqlTypeMetadata.new(sql_type: "integer(8)", type: :integer, limit: 8)
       col = ActiveRecord::ConnectionAdapters::Column.new("price", nil, sql_type_metadata, false, "adverts")
 
       expect(subject.different_to?(col)).to eq(false)
