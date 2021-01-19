927da0b666064827111134fbe3d5489ed126709f
Colin Kelley <colindkelley@gmail.com>
2021-01-18 23:12:09 -0800

    TECH-4814: stub model

--- spec/lib/declare_schema/field_spec_spec.rb
+++ spec/lib/declare_schema/field_spec_spec.rb
@@ -10,33 +10,35 @@ RSpec.describe DeclareSchema::Model::FieldSpec do
     load File.expand_path('prepare_testapp.rb', __dir__)
   end
 
+  let(:model) { double('model', table_options: {}) }
+
   describe '#schema_attributes' do
     describe 'integer 4' do
       it 'returns schema attributes' do
-        subject = described_class.new(Object, :price, :integer, limit: 4, null: false, position: 0)
+        subject = described_class.new(model, :price, :integer, limit: 4, null: false, position: 0)
         expect(subject.schema_attributes).to eq(type: :integer, limit: 4, null: false)
       end
     end
 
     describe 'integer 8' do
       it 'returns schema attributes' do
-        subject = described_class.new(Object, :price, :integer, limit: 8, null: true, position: 2)
+        subject = described_class.new(model, :price, :integer, limit: 8, null: true, position: 2)
         expect(subject.schema_attributes).to eq(type: :integer, limit: 8, null: true)
       end
     end
 
     describe 'bigint' do
       it 'returns schema attributes' do
-        subject = described_class.new(Object, :price, :bigint, null: false, position: 2)
+        subject = described_class.new(model, :price, :bigint, null: false, position: 2)
         expect(subject.schema_attributes).to eq(type: :integer, limit: 8, null: false)
       end
     end
 
     describe 'string' do
       it 'returns schema attributes (including charset/collation iff mysql)' do
-        subject = described_class.new(Object, :title, :string, limit: 100, null: true, collation: 'utf8_general_ci', position: 0)
+        subject = described_class.new(model, :title, :string, limit: 100, null: true, charset: 'utf8mb4', position: 0)
         if defined?(Mysql2)
-          expect(subject.schema_attributes).to eq(type: :string, limit: 100, null: true, charset: 'utf8', collation: 'utf8_general_ci')
+          expect(subject.schema_attributes).to eq(type: :string, limit: 100, null: true, charset: 'utf8mb4', collation: 'utf8mb4_bin')
         else
           expect(subject.schema_attributes).to eq(type: :string, limit: 100, null: true)
         end
@@ -45,19 +47,21 @@ RSpec.describe DeclareSchema::Model::FieldSpec do
 
     describe 'text' do
       it 'returns schema attributes (including charset/collation iff mysql)' do
-        subject = described_class.new(Object, :title, :text, limit: 200, null: true, default: nil, charset: 'utf8', position: 2)
+        subject = described_class.new(model, :title, :text, limit: 200, null: true, default: nil, charset: 'utf8mb4', position: 2)
         if defined?(Mysql2)
-          expect(subject.schema_attributes).to eq(type: :text, limit: 255, null: true, default: nil, charset: 'utf8', collation: 'utf8_general_ci')
+          expect(subject.schema_attributes).to eq(type: :text, limit: 255, null: true, default: nil, charset: 'utf8mb4', collation: 'utf8mb4_bin')
         else
           expect(subject.schema_attributes).to eq(type: :text, limit: 200, null: true, default: nil)
         end
       end
 
       it 'allows a default to be set unless mysql' do
-        subject = described_class.new(Object, :title, :text, limit: 200, null: true, default: 'none', charset: 'utf8', position: 2)
         if defined?(Mysql2)
-          expect { subject.schema_attributes }.to raise_exception(DeclareSchema::MysqlTextMayNotHaveDefault)
+          expect do
+            described_class.new(model, :title, :text, limit: 200, null: true, default: 'none', charset: 'utf8mb4', position: 2)
+          end.to raise_exception(DeclareSchema::MysqlTextMayNotHaveDefault)
         else
+          subject = described_class.new(model, :title, :text, limit: 200, null: true, default: 'none', charset: 'utf8mb4', position: 2)
           expect(subject.schema_attributes).to eq(type: :text, limit: 200, null: true, default: 'none')
         end
       end
@@ -65,22 +69,22 @@ RSpec.describe DeclareSchema::Model::FieldSpec do
 
     describe 'datetime' do
       it 'keeps type as "datetime"' do
-        subject = described_class.new(Object, :created_at, :datetime, null: false, position: 1)
+        subject = described_class.new(model, :created_at, :datetime, null: false, position: 1)
         expect(subject.schema_attributes).to eq(type: :datetime, null: false)
       end
     end
 
     describe 'timestamp' do
       it 'normalizes type to "datetime"' do
-        subject = described_class.new(Object, :created_at, :timestamp, null: true, position: 2)
+        subject = described_class.new(model, :created_at, :timestamp, null: true, position: 2)
         expect(subject.schema_attributes).to eq(type: :datetime, null: true)
       end
     end
   end
 
-  context 'There are no model columns to change' do
+  context 'There are no model, columns to change' do
     it '#different_to should return false for int8 == int8' do
-      subject = described_class.new(Object, :price, :integer, limit: 8, null: false, position: 0)
+      subject = described_class.new(model, :price, :integer, limit: 8, null: false, position: 0)
 
       case Rails::VERSION::MAJOR
       when 4
@@ -95,7 +99,7 @@ RSpec.describe DeclareSchema::Model::FieldSpec do
     end
 
     it '#different_to should return false for bigint == bigint' do
-      subject = described_class.new(Object, :price, :bigint, null: false, position: 0)
+      subject = described_class.new(model, :price, :bigint, null: false, position: 0)
 
       case Rails::VERSION::MAJOR
       when 4
@@ -110,7 +114,7 @@ RSpec.describe DeclareSchema::Model::FieldSpec do
     end
 
     it '#different_to should return false for int8 == bigint' do
-      subject = described_class.new(Object, :price, :integer, limit: 8, null: false, position: 0)
+      subject = described_class.new(model, :price, :integer, limit: 8, null: false, position: 0)
 
       case Rails::VERSION::MAJOR
       when 4
@@ -125,7 +129,7 @@ RSpec.describe DeclareSchema::Model::FieldSpec do
     end
 
     it '#different_to should return false for bigint == int8' do
-      subject = described_class.new(Object, :price, :bigint, null: false, position: 0)
+      subject = described_class.new(model, :price, :bigint, null: false, position: 0)
 
       case Rails::VERSION::MAJOR
       when 4
