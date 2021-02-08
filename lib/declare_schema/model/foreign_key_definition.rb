aceaa714014583d6162c68768c64617153112cae
Colin Kelley <colindkelley@gmail.com>
2021-02-08 07:44:09 -0800

    no-jira: move comment up

--- lib/declare_schema/model/foreign_key_definition.rb
+++ lib/declare_schema/model/foreign_key_definition.rb
@@ -16,10 +16,10 @@ module DeclareSchema
         @parent_table_name = options[:parent_table]&.to_s
         @foreign_key_name = options[:foreign_key]&.to_s || @foreign_key
         @index_name = options[:index_name]&.to_s || model.connection.index_name(model.table_name, column: @foreign_key_name)
-        @constraint_name = options[:constraint_name]&.to_s || @index_name&.to_s || ''
-        @on_delete_cascade = options[:dependent] == :delete
 
         # Empty constraint lets mysql generate the name
+        @constraint_name = options[:constraint_name]&.to_s || @index_name&.to_s || ''
+        @on_delete_cascade = options[:dependent] == :delete
       end
 
       class << self
