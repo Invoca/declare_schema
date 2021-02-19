31273d4e64803d6eb93bfb67944fbac599fc8cad
Drew Caddell <dcaddell@invoca.com>
2021-02-19 10:38:06 -0800

    TECH-5112: Pass args through fields into declare_schema

--- lib/declare_schema/extensions/active_record/fields_declaration.rb
+++ lib/declare_schema/extensions/active_record/fields_declaration.rb
@@ -9,21 +9,7 @@ module DeclareSchema
     deprecate :fields, deprecator: ActiveSupport::Deprecation.new('1.0', 'DeclareSchema')
 
     def fields(table_options = {}, &block)
-      # Any model that calls 'fields' gets DeclareSchema::Model behavior
-      DeclareSchema::Model.mix_in(self)
-
-      # @include_in_migration = false #||= options.fetch(:include_in_migration, true); options.delete(:include_in_migration)
-      @include_in_migration = true
-      @table_options        = table_options
-
-      if block
-        dsl = DeclareSchema::FieldDeclarationDsl.new(self, null: false)
-        if block.arity == 1
-          yield dsl
-        else
-          dsl.instance_eval(&block)
-        end
-      end
+      declare_schema(table_options, &block)
     end
 
     def declare_schema(table_options = {}, &block)
