8f3b081c64ec833cd0f5295955142cc3f5b26bf3
Drew Caddell <dcaddell@invoca.com>
2021-03-02 17:34:37 -0800

    TECH-5112: Stub ThorShell#ask to return a string to stop relying on a redirected text file

--- spec/lib/declare_schema/interactive_primary_key_spec.rb
+++ spec/lib/declare_schema/interactive_primary_key_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'DeclareSchema Migration Generator interactive primary key' do
   end
 
   context 'Using fields' do
-    xit "allows alternate primary keys" do
+    it "allows alternate primary keys" do
       class Foo < ActiveRecord::Base
         fields do
         end
@@ -30,7 +30,7 @@ RSpec.describe 'DeclareSchema Migration Generator interactive primary key' do
         self.primary_key = "id"
       end
 
-      puts "\n\e[45m Please enter 'id' (no quotes) at the next prompt \e[0m"
+      allow_any_instance_of(DeclareSchema::Support::ThorShell).to receive(:ask).with(/one of the rename choices or press enter to keep/) { 'id' }
       generate_migrations '-n', '-m'
       expect(Foo._defined_primary_key).to eq('id')
 
@@ -46,7 +46,7 @@ RSpec.describe 'DeclareSchema Migration Generator interactive primary key' do
           self.primary_key = "foo_id"
         end
 
-        puts "\n\e[45m Please enter 'drop id' (no quotes) at the next prompt \e[0m"
+        allow_any_instance_of(DeclareSchema::Support::ThorShell).to receive(:ask).with(/one of the rename choices or press enter to keep/) { 'drop id' }
         generate_migrations '-n', '-m'
         expect(Foo._defined_primary_key).to eq('foo_id')
 
@@ -79,6 +79,7 @@ RSpec.describe 'DeclareSchema Migration Generator interactive primary key' do
       end
 
       puts "\n\e[45m Please enter 'id' (no quotes) at the next prompt \e[0m"
+      allow_any_instance_of(DeclareSchema::Support::ThorShell).to receive(:ask).with(/one of the rename choices or press enter to keep/) { 'id' }
       generate_migrations '-n', '-m'
       expect(Foo.primary_key).to eq('id')
 
@@ -95,6 +96,7 @@ RSpec.describe 'DeclareSchema Migration Generator interactive primary key' do
         end
 
         puts "\n\e[45m Please enter 'drop id' (no quotes) at the next prompt \e[0m"
+        allow_any_instance_of(DeclareSchema::Support::ThorShell).to receive(:ask).with(/one of the rename choices or press enter to keep/) { 'drop id' }
         generate_migrations '-n', '-m'
         expect(Foo.primary_key).to eq('foo_id')
 
