8a2883e7672555b499f11b09c50dc3dee4ca2b7a
Colin Kelley <colindkelley@gmail.com>
2021-03-06 09:42:21 -0800

    TECH-5338: initial SchemaChange::TableDrop

---
+++ lib/declare_schema/schema_change/table_drop.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative 'base'
+
+module DeclareSchema
+  module SchemaChange
+    class TableDrop < Base
+      def initialize(table_name, add_table_back)
+        @table_name = table_name
+        @add_table_back = add_table_back
+      end
+
+      def up_command
+        "drop_table #{@table_name.to_sym.inspect}"
+      end
+
+      def down_command
+        @add_table_back
+      end
+    end
+  end
+end
