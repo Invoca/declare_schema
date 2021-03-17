6eb26ecd820a5e72a9f7b263529a93e0c4ae7782
Colin Kelley <colindkelley@gmail.com>
2021-03-16 21:34:32 -0700

    TECH-5338: test sql_options

--- spec/lib/declare_schema/schema_change/table_add_spec.rb
+++ spec/lib/declare_schema/schema_change/table_add_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe DeclareSchema::SchemaChange::TableAdd do
   let(:table_name) { 'networks' }
   let(:fields) { [[:string, :title, limit: 255, null: false ], [:boolean, :admin, null: false]] }
   let(:create_table_options) { { id: :primary_key } }
-  let(:sql_options) { nil }
+  let(:sql_options) { '' }
 
   subject { described_class.new(table_name, fields, create_table_options, sql_options: sql_options) }
 
@@ -24,6 +24,20 @@ RSpec.describe DeclareSchema::SchemaChange::TableAdd do
           end
 
         EOS
+      end
+
+      context 'with sql_options' do
+        let(:sql_options) { 'CHARACTER SET utf8mb4' }
+
+        it 'responds with command' do
+          expect(subject.up).to eq(<<~EOS)
+          create_table :networks, id: :primary_key, options: "CHARACTER SET utf8mb4" do |t|
+            t.string  :title, limit: 255, null: false
+            t.boolean :admin, null: false
+          end
+
+          EOS
+        end
       end
     end
 
