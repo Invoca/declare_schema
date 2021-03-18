a240d80640e1e5ba763d0c0eaad261d74d52c5e4
Yinon Rousso <yrousso@invoca.com>
2021-03-18 16:35:22 -0700

    TECH-5413 fix failing test

--- lib/declare_schema/model/foreign_key_definition.rb
+++ lib/declare_schema/model/foreign_key_definition.rb
@@ -19,9 +19,9 @@ module DeclareSchema
         @parent_table_name = options[:parent_table]&.to_s
         @foreign_key_name = options[:foreign_key]&.to_s || @foreign_key
 
-        @constraint_name = options[:constraint_name]&.to_s ||
+        @constraint_name = options[:constraint_name]&.to_s.presence ||
                              options[:index_name]&.to_s ||
-                             IndexDefinition.index_name(@foreign_key_name)
+                             model.connection.index_name(model.table_name, column: @foreign_key_name)
         @on_delete_cascade = options[:dependent] == :delete
       end
 
