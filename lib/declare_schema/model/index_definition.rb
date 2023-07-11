19192589d354afddb8595672f598940c3f699c57
Colin Kelley <colin@invoca.com>
2023-07-11 00:16:35 -0700

    non-production: fix TODO: fields -> columns

--- lib/declare_schema/model/index_definition.rb
+++ lib/declare_schema/model/index_definition.rb
@@ -7,19 +7,18 @@ module DeclareSchema
     class IndexDefinition
       include Comparable
 
-      # TODO: replace `fields` with `columns` and remove alias. -Colin
-      attr_reader :table_name, :fields, :explicit_name, :name, :unique, :where
-      alias columns fields
+      attr_reader :table_name, :columns, :explicit_name, :name, :unique, :where
+      alias fields columns # TODO: change callers to use columns. -Colin
 
       class IndexNameTooLongError < RuntimeError; end
 
       PRIMARY_KEY_NAME = "PRIMARY"
 
-      def initialize(table_name, fields, name: nil, allow_equivalent: false, unique: false, where: nil)
+      def initialize(table_name, columns, name: nil, allow_equivalent: false, unique: false, where: nil)
         @table_name = table_name
-        @fields = Array.wrap(fields).map(&:to_s)
+        @columns = Array.wrap(columns).map(&:to_s)
         @explicit_name = name unless allow_equivalent
-        @name = name || self.class.default_index_name(@table_name, @fields)
+        @name = name || self.class.default_index_name(@table_name, @columns)
         @unique = unique || @name == PRIMARY_KEY_NAME || false
 
         if DeclareSchema.max_index_and_constraint_name_length && @name.length > DeclareSchema.max_index_and_constraint_name_length
@@ -56,10 +55,10 @@ module DeclareSchema
           index_definitions
         end
 
-        def default_index_name(table_name, fields)
+        def default_index_name(table_name, columns)
           index_name = nil
           [:long_index_name, :short_index_name].find do |method_name|
-            index_name = send(method_name, table_name, fields)
+            index_name = send(method_name, table_name, columns)
             if DeclareSchema.max_index_and_constraint_name_length.nil? || index_name.length <= DeclareSchema.max_index_and_constraint_name_length
               break index_name
             end
@@ -101,11 +100,11 @@ module DeclareSchema
       end
 
       def to_key
-        @to_key ||= [table_name, fields, name, unique, where].map(&:to_s)
+        @to_key ||= [table_name, columns, name, unique, where].map(&:to_s)
       end
 
       def settings
-        @settings ||= [table_name, fields, unique].map(&:to_s)
+        @settings ||= [table_name, columns, unique].map(&:to_s)
       end
 
       def hash
@@ -121,7 +120,7 @@ module DeclareSchema
       end
 
       def with_name(new_name)
-        self.class.new(@table_name, @fields, table_name: @table_name, index_name: @index_name, unique: @unique, name: new_name)
+        self.class.new(@table_name, @columns, unique: @unique, name: new_name)
       end
 
       alias eql? ==
